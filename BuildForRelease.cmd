92211cd487f742f6205fedc8683fbe63f6ea01f3
Armah1337 <armah1337@gmail.com>
2019-06-22 23:20:48 +0300

    removed unnecessary batch script

--- BuildForRelease.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-windeployqt --dir "./build" --qmldir . "C:\Maxim\Armah\Downloads\Junk\C++\Qt stuff\Ocelot_1\build-Ocelot_1-Desktop_Qt_5_12_0_MSVC2017_64bit-Release\release\Ocelot_1.exe"
-move "C:\Maxim\Armah\Downloads\Junk\C++\Qt stuff\Ocelot_1\build-Ocelot_1-Desktop_Qt_5_12_0_MSVC2017_64bit-Release\release\Ocelot_1.exe" "./build"
